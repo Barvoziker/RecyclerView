4c4bafcdc7057f302e589f188f3bf8d5ad000fbb
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2022-09-30 08:46:30 +0200

    add images and colors

---
+++ app/src/main/java/com/example/recyclerview/CouleurAdapter.java
@@ -0,0 +1,6 @@
+package com.example.recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CouleurAdapter extends RecyclerView.Adapter<CouleurAdapter.CouleursViewHolder> {
+}
