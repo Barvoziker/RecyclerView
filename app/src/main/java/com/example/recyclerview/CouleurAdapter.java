d2a35997caa4b6cf20d84144ba3c2bd3c9e80c63
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2022-09-30 09:55:01 +0200

    recyclerview

--- app/src/main/java/com/example/recyclerview/CouleurAdapter.java
+++ app/src/main/java/com/example/recyclerview/CouleurAdapter.java
@@ -1,6 +1,60 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CouleurAdapter extends RecyclerView.Adapter<CouleurAdapter.CouleursViewHolder> {
+
+    String[] titre_couleurs;
+    String[] description_couleurs;
+    int[] images;
+    Context context;
+
+    public CouleurAdapter(Context ct, String[] titre_couleurs, String[] description_couleurs, int[] images) {
+        context = ct;
+        this.titre_couleurs = titre_couleurs;
+        this.description_couleurs = description_couleurs;
+        this.images = images;
+    }
+
+    public static class CouleursViewHolder extends RecyclerView.ViewHolder {
+        TextView titre_couleur, description_couleur;
+        ImageView image_couleur;
+
+        public CouleursViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titre_couleur = itemView.findViewById(R.id.titre_couleur);
+            description_couleur = itemView.findViewById(R.id.description_couleur);
+            image_couleur = itemView.findViewById(R.id.image_couleur);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CouleursViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.rv_item, parent, false);
+        return new CouleursViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CouleurAdapter.CouleursViewHolder holder, int position) {
+        holder.titre_couleur.setText(titre_couleurs[position]);
+        holder.description_couleur.setText(description_couleurs[position]);
+        holder.image_couleur.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+
 }
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rv_couleurs;
+
+    String[] titres;
+    String[] descriptions;
+    int[] images = {R.drawable.bleu, R.drawable.rouge, R.drawable.vert, R.drawable.jaune, R.drawable.orange, R.drawable.violet, R.drawable.marron, R.drawable.gris, R.drawable.rose, R.drawable.cyan};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv_couleurs = findViewById(R.id.rv_couleurs);
+
+        titres = getResources().getStringArray(R.array.couleur);
+        descriptions = getResources().getStringArray(R.array.description);
+
+        CouleurAdapter couleurAdapter = new CouleurAdapter(this, titres, descriptions, images);
+        rv_couleurs.setHasFixedSize(true);
+        rv_couleurs.setAdapter(couleurAdapter);
+        rv_couleurs.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
